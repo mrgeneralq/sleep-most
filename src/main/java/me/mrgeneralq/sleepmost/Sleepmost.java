4b09e454956353f4b2c4bd96a380babb36bfa875
RainbowDashLabs <mail@code-fox.de>
2020-11-11 23:38:44 +0100

    relocated main class and use proper logger implementation

--- src/main/java/me/mrgeneralq/sleepmost/Sleepmost.java
+++ src/main/java/me/mrgeneralq/sleepmost/Sleepmost.java
@@ -1,11 +1,10 @@
 package me.mrgeneralq.sleepmost;
-import me.mrgeneralq.sleepmost.eventlisteners.*;
 import me.mrgeneralq.sleepmost.statics.SleepFlagMapper;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import me.mrgeneralq.sleepmost.bstats.Metrics;
 import me.mrgeneralq.sleepmost.commands.SleepmostCommand;
 import me.mrgeneralq.sleepmost.eventlisteners.EntitySpawnEventListener;
 import me.mrgeneralq.sleepmost.eventlisteners.EntityTargetLivingEntityEventListener;
@@ -27,7 +26,7 @@ public class Sleepmost extends JavaPlugin{
 	public void onEnable() {
 
 		if(VersionController.isOldVersion())
-			Bukkit.getLogger().warning("You are using an older Minecraft version that requires a different way of calculating who is asleep.");
+			getLogger().warning("You are using an older Minecraft version that requires a different way of calculating who is asleep.");
 		
 		saveDefaultConfig();
 		
@@ -68,6 +67,6 @@ public class Sleepmost extends JavaPlugin{
 	private void notifyIfNewUpdateExists(IUpdateService updateService) 
 	{
 		if(updateService.hasUpdate())
-			Bukkit.getLogger().info("UPDATE FOUND: A newer version of sleep-most is available to download!");
+			getLogger().info("UPDATE FOUND: A newer version of sleep-most is available to download!");
 	}
 }
