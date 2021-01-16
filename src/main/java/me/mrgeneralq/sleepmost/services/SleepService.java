af033758acee84129d062ccdd538b869ada99c61
Quinten <mrgeneralq@gmail.com>
2021-01-16 23:26:16 +0100

    Skip Delay flag added
    A new flag has been added, skip delay

--- src/main/java/me/mrgeneralq/sleepmost/services/SleepService.java
+++ src/main/java/me/mrgeneralq/sleepmost/services/SleepService.java
@@ -1,10 +1,11 @@
 package me.mrgeneralq.sleepmost.services;
+
 import me.clip.placeholderapi.PlaceholderAPI;
-import me.mrgeneralq.sleepmost.enums.SleepCalculationType;
+import me.mrgeneralq.sleepmost.Sleepmost;
 import me.mrgeneralq.sleepmost.flags.CalculationMethodFlag;
 import me.mrgeneralq.sleepmost.flags.PlayersRequiredFlag;
+import me.mrgeneralq.sleepmost.flags.SkipDelayFlag;
 import me.mrgeneralq.sleepmost.flags.UseAfkFlag;
-import me.mrgeneralq.sleepmost.flags.serialization.SleepCalculationTypeSerialization;
 import me.mrgeneralq.sleepmost.interfaces.*;
 import me.mrgeneralq.sleepmost.statics.DataContainer;
 import me.mrgeneralq.sleepmost.statics.ServerVersion;
@@ -13,7 +14,7 @@ import me.mrgeneralq.sleepmost.events.SleepSkipEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
-
+import org.bukkit.scheduler.BukkitRunnable;
 import static java.util.stream.Collectors.toList;
 
 import java.util.List;
@@ -22,27 +23,41 @@ public class SleepService implements ISleepService {
 
     private final IConfigRepository configRepository;
     private final IConfigService configService;
+    private final SkipDelayFlag skipDelayFlag;
+
 
     private final CalculationMethodFlag calculationMethodFlag;
     private final PlayersRequiredFlag playersRequiredFlag;
     private final UseAfkFlag useAfkFlag;
+    private final Sleepmost plugin;
 
     private static final int
             NIGHT_START_TIME = 12541,
             NIGHT_END_TIME = 23850;
 
-    public SleepService(IConfigService configService, IConfigRepository configRepository, CalculationMethodFlag calculationMethodFlag, PlayersRequiredFlag playersRequiredFlag, UseAfkFlag useAfkFlag){
+    public SleepService(
+            Sleepmost plugin,
+            IConfigService configService,
+            IConfigRepository configRepository,
+            CalculationMethodFlag calculationMethodFlag,
+            PlayersRequiredFlag playersRequiredFlag,
+            UseAfkFlag useAfkFlag,
+            SkipDelayFlag skipDelayFlag)
+    {
+
+        this.plugin = plugin;
         this.configService = configService;
         this.configRepository = configRepository;
 
         this.calculationMethodFlag = calculationMethodFlag;
         this.playersRequiredFlag = playersRequiredFlag;
         this.useAfkFlag = useAfkFlag;
+        this.skipDelayFlag = skipDelayFlag;
     }
 
     @Override
     public boolean enabledForWorld(World world) {
-       return configRepository.containsWorld(world);
+        return configRepository.containsWorld(world);
     }
 
     @Override
@@ -68,11 +83,10 @@ public class SleepService implements ISleepService {
 
     @Override
     public int getPlayersSleepingCount(World world) {
-        if(ServerVersion.CURRENT_VERSION.sleepCalculatedDifferently()){
+        if (ServerVersion.CURRENT_VERSION.sleepCalculatedDifferently()) {
             return DataContainer.getContainer().getSleepingPlayers(world).size();
-        }
-        else {
-            return (int) (world.getPlayers().stream().filter(Player::isSleeping).count()+1);
+        } else {
+            return (int) (world.getPlayers().stream().filter(Player::isSleeping).count() + 1);
         }
 
     }
@@ -80,26 +94,15 @@ public class SleepService implements ISleepService {
     @Override
     public int getRequiredPlayersSleepingCount(World world) {
 
-        /*SleepCalculationType sleepCalculationType = SleepCalculationType.PLAYERS_REQUIRED;
-
-        try{
-            String enumName = String.format("%s%s", this.calculationMethodFlag.getValueAt(world), "_REQUIRED");
-            enumName = enumName.toUpperCase();
-
-            sleepCalculationType = SleepCalculationType.valueOf(enumName);
-        }
-        catch (Exception ex){}*/
-
         int requiredCount;
 
-        switch (this.calculationMethodFlag.getValueAt(world))
-        {
+        switch (this.calculationMethodFlag.getValueAt(world)) {
             case PERCENTAGE_REQUIRED:
-                requiredCount =  (int) Math.ceil(getPlayerCountInWorld(world) * getPercentageRequired(world));
+                requiredCount = (int) Math.ceil(getPlayerCountInWorld(world) * getPercentageRequired(world));
                 break;
             case PLAYERS_REQUIRED:
                 int requiredPlayersInConfig = this.playersRequiredFlag.getValueAt(world);
-                requiredCount = (requiredPlayersInConfig <= getPlayerCountInWorld(world)) ? requiredPlayersInConfig: getPlayerCountInWorld(world);
+                requiredCount = (requiredPlayersInConfig <= getPlayerCountInWorld(world)) ? requiredPlayersInConfig : getPlayerCountInWorld(world);
                 break;
             default:
                 requiredCount = 0;
@@ -115,7 +118,7 @@ public class SleepService implements ISleepService {
         List<Player> allPlayers = world.getPlayers();
 
         //check if exempt flag is enabled
-        if(configRepository.getUseExempt(world)){
+        if (configRepository.getUseExempt(world)) {
             allPlayers = allPlayers.stream()
                     .filter(p -> !p.hasPermission("sleepmost.exempt"))
                     .collect(toList());
@@ -123,33 +126,43 @@ public class SleepService implements ISleepService {
         boolean afkFlagEnabled = this.useAfkFlag.getValueAt(world);
 
         //check if user is afk
-        if(afkFlagEnabled && Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null && Bukkit.getPluginManager().getPlugin("Essentials") != null)
-               allPlayers = allPlayers.stream()
-                       .filter(p -> PlaceholderAPI.setPlaceholders(p, "%essentials_afk%").equalsIgnoreCase("no"))
-                       .collect(toList());
+        if (afkFlagEnabled && Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null && Bukkit.getPluginManager().getPlugin("Essentials") != null)
+            allPlayers = allPlayers.stream()
+                    .filter(p -> PlaceholderAPI.setPlaceholders(p, "%essentials_afk%").equalsIgnoreCase("no"))
+                    .collect(toList());
 
-            return (allPlayers.size() > 0) ? allPlayers.size() : 1;
+        return (allPlayers.size() > 0) ? allPlayers.size() : 1;
     }
 
     @Override
     public void resetDay(World world, String lastSleeperName, String lastSleeperDisplayName) {
-        SleepSkipCause cause = SleepSkipCause.UNKNOWN;
 
-        if (this.isNight(world)) {
-            cause = SleepSkipCause.NIGHT_TIME;
-            world.setTime(configService.getResetTime());
-        } else if(world.isThundering()){
-            cause = SleepSkipCause.STORM;
-        }
-        
-        world.setThundering(false);
-        world.setStorm(false);
-        Bukkit.getServer().getPluginManager().callEvent(new SleepSkipEvent(world, cause, lastSleeperName, lastSleeperDisplayName));
+        /*here we need to add a scheduler that runs all of this code after if the value of the skip-delay flag is higher then 0. If it is,
+         * we run the code to skip the night and reset the time. ALERT: we need to check again in the condition if the required players are asleep, if so we skip.
+         * we also need to check if time is not already skipped, so if time = day, do not run code (return)
+         */
+
+        int skipDelay = skipDelayFlag.getValueAt(world);
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+
+                if (!resetRequired(world))
+                    return;
+
+                if (!sleepPercentageReached(world))
+                    return;
+
+                executeSleepReset(world, lastSleeperName, lastSleeperDisplayName);
+
+            }
+        }.runTaskLaterAsynchronously(plugin, skipDelay * 20L);
     }
 
     @Override
     public boolean resetRequired(World world) {
-    	return isNight(world) || world.isThundering();
+        return isNight(world) || world.isThundering();
     }
 
     @Override
@@ -159,7 +172,7 @@ public class SleepService implements ISleepService {
 
     @Override
     public SleepSkipCause getSleepSkipCause(World world) {
-    	return isNight(world) ? SleepSkipCause.NIGHT_TIME : SleepSkipCause.STORM;
+        return isNight(world) ? SleepSkipCause.NIGHT_TIME : SleepSkipCause.STORM;
     }
 
     @Override
@@ -175,5 +188,22 @@ public class SleepService implements ISleepService {
     @Override
     public void disableForWorld(World world) {
         configRepository.disableForWorld(world);
+    }
+
+
+    // this method will invoke the reset functionality for the day
+    private void executeSleepReset(World world, String lastSleeperName, String lastSleeperDisplayName) {
+        SleepSkipCause cause = SleepSkipCause.UNKNOWN;
+
+        if (this.isNight(world)) {
+            cause = SleepSkipCause.NIGHT_TIME;
+            world.setTime(configService.getResetTime());
+        } else if (world.isThundering()) {
+            cause = SleepSkipCause.STORM;
+        }
+
+        world.setThundering(false);
+        world.setStorm(false);
+        Bukkit.getServer().getPluginManager().callEvent(new SleepSkipEvent(world, cause, lastSleeperName, lastSleeperDisplayName));
     }
 }
