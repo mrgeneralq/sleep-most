49bd4564bf7e7ef45419db22837be90c82d25001
David <darknight4123@gmail.com>
2021-06-04 21:26:18 +0300

    Fixed the bug where the heal flag throws an exception

--- src/main/java/me/mrgeneralq/sleepmost/statics/ServerVersion.java
+++ src/main/java/me/mrgeneralq/sleepmost/statics/ServerVersion.java
@@ -2,6 +2,7 @@ package me.mrgeneralq.sleepmost.statics;
 
 import org.apache.commons.lang.WordUtils;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
@@ -12,12 +13,10 @@ public enum ServerVersion
     UNKNOWN, V1_8, V1_9, V1_10, V1_11, V1_12, V1_13, V1_14, V1_15, V1_16, V1_17;
 
     private final String name;
-
-    //settings
+    private MaxHPHealer maxHPHealer;
     private boolean supportsHexColors;
     private boolean supportsTitles;
 
-    //the cached current server version
     public static final ServerVersion CURRENT_VERSION;
 
     public static final String UPDATE_URL = "https://www.spigotmc.org/resources/sleep-most-1-8-1-16-1-configurable-messages-and-percentage.60623/";
@@ -25,52 +24,52 @@ public enum ServerVersion
     //cached for internal usage
     private static final ServerVersion[] VALUES = values();
 
-    static
-    {
+    static {
         CURRENT_VERSION = computeServerVersion();
 
         forVersionsFrom(V1_16, version -> version.supportsHexColors = true);
         forVersionsFrom(V1_14, version -> version.supportsTitles = true);
+        forVersionsUntil(V1_9, version -> version.maxHPHealer = MaxHPHealer.LEGACY_HEALER);
+        forVersionsFrom(V1_9, version -> version.maxHPHealer = MaxHPHealer.UPDATED_HEALER);
     }
-    ServerVersion()
-    {
+    ServerVersion() {
         this.name = WordUtils.capitalizeFully(name().substring(1).toLowerCase().replace('_', '.'));
     }
-    public String getName()
-    {
+
+    public String getName() {
         return this.name;
     }
-    public boolean supportsHexColors()
-    {
+
+    public boolean supportsHexColors() {
         return this.supportsHexColors;
     }
-    public boolean supportsTitles()
-    {
+
+    public boolean supportsTitles() {
         return this.supportsTitles;
     }
 
+    public void healToMaxHP(Player player) {
+        this.maxHPHealer.heal(player);
+    }
+
     //Setup methods
-    private static ServerVersion computeServerVersion()
-    {
+    private static ServerVersion computeServerVersion() {
         return Arrays.stream(VALUES)
                 .filter(version -> Bukkit.getVersion().contains(version.getName()))
                 .findFirst()
                 .orElse(UNKNOWN);
     }
-    private static void forVersionsFrom(ServerVersion minimum, Consumer<ServerVersion> action)
-    {
+    private static void forVersionsFrom(ServerVersion minimum, Consumer<ServerVersion> action) {
         versionsStream()
                 .filter(version -> version.ordinal() >= minimum.ordinal())
                 .forEach(action);
     }
-    private static void forVersionsUntil(ServerVersion maximum, Consumer<ServerVersion> action)
-    {
+    private static void forVersionsUntil(ServerVersion maximum, Consumer<ServerVersion> action) {
         versionsStream()
                 .filter(version -> version.ordinal() <= maximum.ordinal())
                 .forEach(action);
     }
-    private static Stream<ServerVersion> versionsStream()
-    {
+    private static Stream<ServerVersion> versionsStream() {
         return Arrays.stream(VALUES)
                 .filter(version -> version != UNKNOWN);
     }
