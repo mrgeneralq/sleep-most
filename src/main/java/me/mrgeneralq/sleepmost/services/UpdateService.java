b52ba4454e394d0add5b3b888fac90658037c2e9
Nozemi <nozemi95@gmail.com>
2022-01-08 20:05:17 +0100

    Update Checking Service Changes - Added a class Version with Version#isBiggerThan(), to compare two versions - Removed the empty - Added some comments un UpdateServiceTest#hasUpdate() to make it clear what it's comparing to - Cleaned up UpdateService#hasUpdate(String) and made use of Version-class

--- src/main/java/me/mrgeneralq/sleepmost/services/UpdateService.java
+++ src/main/java/me/mrgeneralq/sleepmost/services/UpdateService.java
@@ -4,22 +4,18 @@ import me.mrgeneralq.sleepmost.interfaces.IConfigService;
 import me.mrgeneralq.sleepmost.Sleepmost;
 import me.mrgeneralq.sleepmost.interfaces.IUpdateRepository;
 import me.mrgeneralq.sleepmost.interfaces.IUpdateService;
-
-import java.util.Arrays;
-import java.util.List;
-
-import static java.util.stream.Collectors.toList;
+import me.mrgeneralq.sleepmost.utils.Version;
 
 public class UpdateService implements IUpdateService {
 
-    private final IUpdateRepository updateRepository;
     private final Sleepmost main;
     private final IConfigService configService;
+    private final IUpdateRepository updateRepository;
 
     public UpdateService(IUpdateRepository updateRepository, Sleepmost main, IConfigService configService) {
-        this.updateRepository = updateRepository;
         this.main = main;
         this.configService = configService;
+        this.updateRepository = updateRepository;
     }
 
     @Override
@@ -37,24 +33,7 @@ public class UpdateService implements IUpdateService {
         if (latestVersion.equals(localVersion))
             return false;
 
-        List<Integer> splitCurrentVersion = getComponents(localVersion);
-        List<Integer> splitLatestVersion = getComponents(latestVersion);
-
-        equalizeSizes(splitLatestVersion, splitCurrentVersion);
-
-        for (int i = 0; i < splitLatestVersion.size(); i++) {
-            Integer current = splitCurrentVersion.get(i);
-            Integer latest = splitLatestVersion.get(i);
-
-            // latest is bigger
-            if (latest > current)
-                return true;
-
-            // latest is lower
-            if (latest < current)
-                return false;
-        }
-        return false;
+        return new Version(latestVersion).isBiggerThan(new Version(localVersion));
     }
 
     @Override
@@ -65,22 +44,5 @@ public class UpdateService implements IUpdateService {
     @Override
     public String getCachedUpdateVersion() {
         return updateRepository.getLatestVersion();
-    }
-
-    private static List<Integer> getComponents(String version) {
-        return Arrays.stream(version.split("\\."))
-                .map(Integer::parseInt)
-                .collect(toList());
-    }
-
-    private static void equalizeSizes(List<Integer> splitLocalVersion, List<Integer> splitRemoteVersion) {
-        int localSize = splitLocalVersion.size();
-        int cachedSize = splitRemoteVersion.size();
-
-        int zerosAmount = Math.max(localSize, cachedSize) - Math.min(localSize, cachedSize);
-        List<Integer> smallerList = localSize < cachedSize ? splitLocalVersion : splitRemoteVersion;
-
-        for (int i = 1; i <= zerosAmount; i++)
-            smallerList.add(0);
     }
 }
