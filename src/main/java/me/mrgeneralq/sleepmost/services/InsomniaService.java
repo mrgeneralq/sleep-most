941fdacbc500c2372b3364908d4328a0d26a9f26
MrGeneralQ <mrgeneralq@gmail.com>
2022-09-01 11:58:26 +0200

    hotfix for players not being registered

--- src/main/java/me/mrgeneralq/sleepmost/services/InsomniaService.java
+++ src/main/java/me/mrgeneralq/sleepmost/services/InsomniaService.java
@@ -2,8 +2,11 @@ package me.mrgeneralq.sleepmost.services;
 
 import me.mrgeneralq.sleepmost.interfaces.IInsomniaService;
 import me.mrgeneralq.sleepmost.interfaces.ISleepMostPlayerService;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
+
+import java.util.stream.Collectors;
 
 public class InsomniaService implements IInsomniaService {
 
@@ -26,12 +29,16 @@ public class InsomniaService implements IInsomniaService {
 
     @Override
     public void disableInsomnia(Player player, World world) {
+
+        if(!player.isOnline())
+            return;
+
         this.sleepMostPlayerService.getPlayer(player).setInsomniaStatus(world, false);
     }
 
     @Override
     public void disableInsomnia(World world) {
-        for(Player p: world.getPlayers())
+        for(Player p: world.getPlayers().stream().filter(OfflinePlayer::isOnline).collect(Collectors.toList()))
             this.disableInsomnia(p, world);
     }
 
