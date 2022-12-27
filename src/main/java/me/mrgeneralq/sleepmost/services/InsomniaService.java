491e72c001b5b1db12c752889b68d5372ad4cb7f
mrgeneralq <mrgeneralq@gmail.com>
2022-12-27 12:30:08 +0100

    added check for real players in InsomniaService

--- src/main/java/me/mrgeneralq/sleepmost/services/InsomniaService.java
+++ src/main/java/me/mrgeneralq/sleepmost/services/InsomniaService.java
@@ -2,6 +2,7 @@ package me.mrgeneralq.sleepmost.services;
 
 import me.mrgeneralq.sleepmost.interfaces.IInsomniaService;
 import me.mrgeneralq.sleepmost.interfaces.ISleepMostPlayerService;
+import me.mrgeneralq.sleepmost.utils.PlayerUtils;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -18,17 +19,27 @@ public class InsomniaService implements IInsomniaService {
 
     @Override
     public void enableInsomnia(Player player, World world) {
+        if(!PlayerUtils.isRealPlayer(player)){
+            return;
+        }
         this.sleepMostPlayerService.getPlayer(player).setInsomniaStatus(world, true);
     }
 
     @Override
     public void enableInsomnia(World world) {
-        for(Player p: world.getPlayers())
+        for(Player p: world.getPlayers()){
+            if(!PlayerUtils.isRealPlayer(p)){
+                continue;
+            }
             this.enableInsomnia(p, world);
+        }
     }
 
     @Override
     public void disableInsomnia(Player player, World world) {
+
+        if(!PlayerUtils.isRealPlayer(player))
+            return;
 
         if(!player.isOnline())
             return;
@@ -38,12 +49,20 @@ public class InsomniaService implements IInsomniaService {
 
     @Override
     public void disableInsomnia(World world) {
-        for(Player p: world.getPlayers().stream().filter(OfflinePlayer::isOnline).collect(Collectors.toList()))
+        for(Player p: world.getPlayers().stream().filter(OfflinePlayer::isOnline).collect(Collectors.toList())){
+            if(!PlayerUtils.isRealPlayer(p)){
+                continue;
+            }
             this.disableInsomnia(p, world);
+        }
     }
 
     @Override
     public boolean hasInsomniaEnabled(Player player){
+
+        if(!PlayerUtils.isRealPlayer(player))
+            return false;
+
         return this.sleepMostPlayerService.getPlayer(player).getInsomniaStatus(player.getWorld());
     }
 }
