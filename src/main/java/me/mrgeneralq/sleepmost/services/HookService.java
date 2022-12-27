8d7b95c855e58506af849f5e896243bb117a59db
mrgeneralq <mrgeneralq@gmail.com>
2022-12-27 17:03:42 +0100

    missing hook service changes

---
+++ src/main/java/me/mrgeneralq/sleepmost/services/HookService.java
@@ -0,0 +1,47 @@
+package me.mrgeneralq.sleepmost.services;
+
+import me.mrgeneralq.sleepmost.enums.HookType;
+import me.mrgeneralq.sleepmost.interfaces.IHookRepository;
+import me.mrgeneralq.sleepmost.interfaces.IHookService;
+import me.mrgeneralq.sleepmost.models.Hook;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+
+public class HookService implements IHookService {
+
+    private final PluginManager pluginManager;
+    private final IHookRepository hookRepository;
+
+    public HookService(PluginManager pluginManager, IHookRepository hookRepository) {
+        this.pluginManager = pluginManager;
+        this.hookRepository = hookRepository;
+    }
+
+
+    @Override
+    public void attemptRegister(Hook hook)  {
+
+        Bukkit.getLogger().info(String.format("Hooking to %s" , hook.getName()));
+
+        if(!this.pluginManager.isPluginEnabled(hook.getName())){
+            Bukkit.getLogger().info(hook.getName() + " NOT registered");
+            return;
+        }
+
+        for(Hook dependency: hook.getDependencies()){
+            if(!this.pluginManager.isPluginEnabled(hook.getName())){
+                Bukkit.getLogger().severe(String.format("Missing required dependency {%s} for {%s}", dependency.getName(), hook.getName()));
+                return;
+            }
+        }
+
+        this.hookRepository.addOrUpdate(hook);
+        Bukkit.getLogger().info(String.format("Hooked to %s" , hook.getName()));
+    }
+
+    @Override
+    public boolean isRegistered(HookType hookType) {
+        return this.hookRepository.exists(hookType);
+    }
+
+}
