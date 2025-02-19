3a0d9c5c7d21e4b08497c2386ef644f9ac2b21ca
MrGeneralQ <mrgeneralq@gmail.com>
2025-02-19 21:28:21 +0100

    gsit 2.2 fixed

--- src/main/java/me/mrgeneralq/sleepmost/hooks/GsitHook.java
+++ src/main/java/me/mrgeneralq/sleepmost/hooks/GsitHook.java
@@ -2,6 +2,7 @@ package me.mrgeneralq.sleepmost.hooks;
 
 import dev.geco.gsit.api.GSitAPI;
 import dev.geco.gsit.object.GStopReason;
+import dev.geco.gsit.object.IGPose;
 import me.mrgeneralq.sleepmost.enums.SleepMostHook;
 import me.mrgeneralq.sleepmost.models.Hook;
 import org.bukkit.block.BlockFace;
@@ -15,10 +16,15 @@ public class GsitHook extends Hook {
     }
 
     public void setSleepingPose(Player player, boolean sleeping){
-            if(sleeping)
+
+        if(sleeping){
             GSitAPI.createPose(player.getLocation().getBlock().getRelative(BlockFace.DOWN), player, Pose.SLEEPING);
-            else {
-                GSitAPI.removePose(player, GStopReason.GET_UP);
-            }
+        }
+        else {
+            IGPose pose =  GSitAPI.getPoseByPlayer(player);
+            if(pose == null)
+                return;
+            GSitAPI.removePose(pose, GStopReason.GET_UP);
+        }
     }
 }
